57438780013fcf3e9ed43078b2ce0916bab3e123
twkesq <twkesqolek@gmail.com>
2024-07-12 21:00:44 +0300

    fixes gitignore

---
+++ commands/testHandler.py
@@ -0,0 +1,8 @@
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+
+# @Client.on_message(filters.command("пробив", "."))
+# async def test(client, message: Message):
+#     # await client.get_users()
+#     await message.reply(message)
